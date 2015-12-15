87064bba17786c7156566996050bda2b334bc433
Julien Batonnet <julien.batonnet@gmail.com>
2015-12-16 00:07:10 +0100

    Refactored log output display (not fully working yet)

---
+++ FlowTomator.Desktop/MessageSourceFilterConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using FlowTomator.Common;
+
+namespace FlowTomator.Desktop
+{
+    public class MessageSourceFilterConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values == null || values.Length != 2)
+                throw new NotSupportedException();
+
+            ObservableCollection<LogMessage> messages = values[0] as ObservableCollection<LogMessage>;
+            LogVerbosity? verbosity = values[1] as LogVerbosity?;
+            if (messages == null || verbosity == null)
+                throw new NotSupportedException();
+            
+            CollectionViewSource collectionViewSource = new CollectionViewSource() { Source = messages as ObservableCollection<LogMessage> };
+
+            ICollectionView collectionView = collectionViewSource.View;
+            collectionView.Filter = i => ((LogMessage)i).Verbosity >= verbosity.Value;
+
+            return collectionView;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
