a79cf237acef4b494118cbb690094397e713a519
Julien Batonnet <julien.batonnet@gmail.com>
2015-12-17 09:16:06 +0100

    Fix a bug in DynamicEnumerables

--- FlowTomator.Desktop/Editors/Variables/DynamicEnumerableVariableEditor.xaml.cs
+++ FlowTomator.Desktop/Editors/Variables/DynamicEnumerableVariableEditor.xaml.cs
@@ -21,13 +21,7 @@ namespace FlowTomator.Desktop
 {
     public partial class DynamicEnumerableVariableEditor : UserControl
     {
-        public VariableInfo VariableInfo
-        {
-            get
-            {
-                return DataContext as VariableInfo;
-            }
-        }
+        public VariableInfo VariableInfo { get; private set; }
         public Variable Variable
         {
             get
@@ -71,6 +65,14 @@ namespace FlowTomator.Desktop
         public DynamicEnumerableVariableEditor()
         {
             InitializeComponent();
+
+            DataContextChanged += DynamicEnumerableVariableEditor_DataContextChanged;
+        }
+
+        private void DynamicEnumerableVariableEditor_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (e.NewValue is VariableInfo)
+                VariableInfo = e.NewValue as VariableInfo;
         }
     }
 }
--- FlowTomator.Desktop/Windows/References.xaml.cs
+++ FlowTomator.Desktop/Windows/References.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -14,14 +16,41 @@ using System.Windows.Shapes;
 
 namespace FlowTomator.Desktop
 {
-    /// <summary>
-    /// Interaction logic for References.xaml
-    /// </summary>
     public partial class References : Window
     {
+        public ObservableDictionary<Assembly, bool> Assemblies { get; } = new ObservableDictionary<Assembly, bool>();
+        public ObservableDictionary<FileInfo, bool> Flows { get; } = new ObservableDictionary<FileInfo, bool>();
+
         public References()
         {
             InitializeComponent();
+
+            // Load assemblies
+            foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
+                AnalyzeAssembly(assembly);
+
+        }
+
+        private void AnalyzeAssembly(Assembly assembly)
+        {
+            /*IEnumerable<Type> nodeTypes = Enumerable.Empty<Type>();
+
+            //nodeTypes = assembly.GetModules().SelectMany(m => m.GetTypes());
+            nodeTypes = assembly.GetTypes();
+
+            nodeTypes = nodeTypes.Where(t => !t.IsAbstract && t.IsSubclassOf(typeof(Node)) && t != typeof(Flow) && !t.IsSubclassOf(typeof(Flow)))
+                                 .Where(t => t.GetConstructor(Type.EmptyTypes) != null);
+
+            foreach (Type type in nodeTypes)
+            {
+                NodeTypeInfo nodeType = NodeTypeInfo.From(type);
+                NodeCategoryInfo nodeCategory = NodeCategories.FirstOrDefault(c => c.Category == nodeType.Category);
+
+                if (nodeCategory == null)
+                    NodeCategories.Add(nodeCategory = new NodeCategoryInfo(nodeType.Category));
+
+                nodeCategory.Nodes.Add(nodeType);
+            }*/
         }
     }
 }
