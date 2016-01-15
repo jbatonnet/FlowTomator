162dc721363f5eee0ae69e200a97c62fe354b1b4
Julien Batonnet <julien.batonnet@gmail.com>
2016-01-15 15:15:32 +0100

    Rename Sftp plugin to Ssh.

--- FlowTomator.Ssh/Properties/AssemblyInfo.cs
+++ FlowTomator.Ssh/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FlowTomator.Sftp")]
+[assembly: AssemblyTitle("FlowTomator.Ssh")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FlowTomator.Sftp")]
+[assembly: AssemblyProduct("FlowTomator.Ssh")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
