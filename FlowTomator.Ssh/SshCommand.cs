cacc689dbe24062f4b92eb9a92f5796144250c13
Stephane Marquis <stephane.marquis@ubisoft.com>
2017-11-23 10:28:50 -0500

    Adding the possibility to use SSL Certificate

--- FlowTomator.Ssh/SshCommand.cs
+++ FlowTomator.Ssh/SshCommand.cs
@@ -12,14 +12,29 @@ namespace FlowTomator.Ssh
     [Node("Ssh command", "Ssh", "Dumps the specified data into the specified file")]
     public class SshCommand : Task
     {
+        public enum AuthenticationType
+        {
+            Password,
+            Certificate
+        }
+
         public override IEnumerable<Variable> Inputs
         {
             get
             {
                 yield return host;
                 yield return port;
-                yield return user;
-                yield return password;
+                yield return authentication;
+                if (authentication.Value == AuthenticationType.Password)
+                {
+                    yield return user;
+                    yield return password;
+                }
+                else if (authentication.Value == AuthenticationType.Certificate)
+                { 
+                    yield return certificate;
+                    yield return passphrase;
+                }
                 yield return command;
             }
         }
@@ -29,15 +44,27 @@ namespace FlowTomator.Ssh
         private Variable<string> user = new Variable<string>("User", "root", "The user used to log in the specified host");
         private Variable<string> password = new Variable<string>("Password", "", "The password used to authenticate the specified user");
         private Variable<string> command = new Variable<string>("Command", "pwd", "The command to be executed on the remote host");
+        private Variable<AuthenticationType> authentication = new Variable<AuthenticationType>("Authentication", AuthenticationType.Password);
+        private Variable<FileInfo> certificate = new Variable<FileInfo>("SSL Certificate", null, "SSL Certificate to be used for the connection.");
+        private Variable<string> passphrase = new Variable<string>("Passphrase","","The certificate passphrase");
 
         public override NodeResult Run()
         {
-            if (string.IsNullOrEmpty(host.Value) || string.IsNullOrEmpty(user.Value) || string.IsNullOrEmpty(command.Value))
+            if (authentication.Value == AuthenticationType.Password && (string.IsNullOrEmpty(host.Value) || string.IsNullOrEmpty(user.Value) || string.IsNullOrEmpty(command.Value)))
+                return NodeResult.Fail;
+            if(authentication.Value == AuthenticationType.Certificate && (string.IsNullOrEmpty(host.Value) || string.IsNullOrEmpty(command.Value) || certificate.Value == null))
                 return NodeResult.Fail;
 
             try
             {
-                SshClient sshClient = new SshClient(host.Value, port.Value, user.Value, password.Value);
+                SshClient sshClient;
+                if (authentication.Value == AuthenticationType.Certificate && string.IsNullOrEmpty(passphrase.Value))
+                    sshClient = new SshClient(host.Value, port.Value, user.Value, new PrivateKeyFile(certificate.Value.FullName));
+                else if (authentication.Value == AuthenticationType.Certificate && !string.IsNullOrEmpty(passphrase.Value))
+                    sshClient = new SshClient(host.Value, port.Value, user.Value, new PrivateKeyFile(certificate.Value.FullName, passphrase.Value));
+                else
+                    sshClient = new SshClient(host.Value, port.Value, user.Value, password.Value);
+
                 sshClient.Connect();
 
                 Renci.SshNet.SshCommand sshCommand = sshClient.RunCommand(command.Value);
